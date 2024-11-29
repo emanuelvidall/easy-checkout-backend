912e8c93ae1f0507921eb50fe801d16b4b14f272
Emanuel Vidal <emanuelvidalrm@gmail.com>
2024-11-29 16:21:39 -0300

    :sparkles: product resolver

--- src/modules/product/product.resolver.ts
+++ src/modules/product/product.resolver.ts
@@ -12,10 +12,101 @@ export class ProductResolver {
     return this.productService.getAllProducts();
   }
 
+  //Example query to get all products:
+  //   query {
+  //   getProducts {
+  //     id
+  //     name
+  //     description
+  //     price
+  //     imageURL
+  //     createdAt
+  //   }
+  // }
+
   @Mutation(() => Product)
   async createProduct(
     @Args('createProductInput') createProductInput: CreateProductInput,
   ): Promise<Product> {
     return this.productService.createProduct(createProductInput);
   }
+
+  //Example of mutation to create a product:
+  //   mutation {
+  //   createProduct(
+  //     createProductInput: {
+  //       name: "Smartphone"
+  //       description: "A high-end smartphone"
+  //       price: 999.99
+  //       imageURL: "http://example.com/smartphone.jpg"
+  //     }
+  //   ) {
+  //     id
+  //     name
+  //     description
+  //     price
+  //     imageURL
+  //     createdAt
+  //   }
+  // }
+
+  @Query(() => Product)
+  async getProductById(@Args('id') id: string): Promise<Product> {
+    return this.productService.getProductById(id);
+  }
+
+  // Example of query to get a product by ID:
+  // query {
+  // getProductById(id: "83a3f0e6-dce0-4ff5-9fc6-0abd9eb7a36a") {
+  //   id
+  //   name
+  //   description
+  //   price
+  //   createdAt
+  // }
+  // }
+
+  @Mutation(() => Product)
+  async updateProduct(
+    @Args('id') id: string,
+    @Args('updateProductInput') updateProductInput: CreateProductInput,
+  ): Promise<Product> {
+    return this.productService.updateProduct(id, updateProductInput);
+  }
+
+  //Example of a mutation to update a product:
+  //   mutation {
+  //   updateProduct(
+  //     id: "83a3f0e6-dce0-4ff5-9fc6-0abd9eb7a36a"
+  //     updateProductInput: {
+  //       name: "Updated Smartphone"
+  //       description: "An updated high-end smartphone"
+  //       price: 899.99
+  //       imageURL: "http://example.com/updated-smartphone.jpg"
+  //     }
+  //   ) {
+  //     id
+  //     name
+  //     description
+  //     price
+  //     imageURL
+  //     createdAt
+  //   }
+  // }
+
+  @Mutation(() => Product)
+  async deleteProduct(@Args('id') id: string): Promise<Product> {
+    return this.productService.deleteProduct(id);
+  }
+
+  //Example of mutation to delete product:
+  //   mutation {
+  //   deleteProduct(id: "83a3f0e6-dce0-4ff5-9fc6-0abd9eb7a36a") {
+  //     id
+  //     name
+  //     description
+  //     price
+  //     createdAt
+  //   }
+  // }
 }
